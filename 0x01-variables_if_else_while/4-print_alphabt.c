00c170cd7bc12dfebeab75701bb39d440a82f7ad
Nel <n_wanyama@yahoo.com>
2022-12-11 10:14:08 +0000

    Print alphabet, skip specified letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 -> success
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	for (; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
