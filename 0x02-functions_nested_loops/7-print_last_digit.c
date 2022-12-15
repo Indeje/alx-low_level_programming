8dc0de3e453a56be70aca82f896368681ae7de6f
Nel <n_wanyama@yahoo.com>
2022-12-15 02:53:16 +0000

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Print last digit of number
+ * @n: Number
+ *
+ * Return: last digit of number if successful, -1 if failure
+ */
+
+int print_last_digit(int n)
+{
+	int rem = -1;
+
+	rem = n % 10;
+
+	return (rem);
+}
