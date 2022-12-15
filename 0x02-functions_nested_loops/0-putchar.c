334f962ed23e893e357976a2d6a4cb2a45619274
Nel <n_wanyama@yahoo.com>
2022-12-15 01:13:57 +0000

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,22 @@
-#include 
+#include "main.h"
+
+/**
+ * main - entry point -> print _putchar
+ *
+ * Return: 0 -> success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
