480680a217deae669f7e26324fe70517c9a8c99d
Nel <n_wanyama@yahoo.com>
2022-12-15 01:48:12 +0000

    Print lowercase alphabet 10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabet 10times
+ *
+ * Return: void -> no return value
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while(i < 10)
+	{
+		char c = 'a';
+
+		for(; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+
+		i++;
+	}
+}
