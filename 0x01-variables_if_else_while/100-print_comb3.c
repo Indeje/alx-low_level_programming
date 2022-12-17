55347cd98212c17339c5a160b26776138682a71b
Nel <n_wanyama@yahoo.com>
2022-12-17 12:29:18 +0000

    Print all posssible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main -> prints single-digit numbers
+ *
+ * Return: 0 -> success
+ */
+
+int main(void)
+{
+	int i = '0', j = '1';
+
+	for (; i <= '8'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			if ((i != j) && (i < j))
+			{
+				putchar(i);
+				putchar(j);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
