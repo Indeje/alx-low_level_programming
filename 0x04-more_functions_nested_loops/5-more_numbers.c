df018beb387469186df38bfbb4503455dba0a20a
Nel <n_wanyama@yahoo.com>
2022-12-18 13:25:11 +0000

    Print 10times the numbers 0-14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,16 @@
 
 void more_numbers(void)
 {
-	int a = 0;
+	int a = '0', b = '1', i = 0, count = 0;
 
-	for (; a < 10; a++)
+	for (; i < 10; i++)
 	{
-		int i = '0';
-
-		for (; i <= '14'; i++)
-			_putchar(i);
+		for (; count <= 14; count++)
+		{
+			if (count > 9)
+				_putchar(b);
+			_putchar(a);
+		}
 		_putchar('\n');
 	}
 }
