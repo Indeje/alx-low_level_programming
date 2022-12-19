cab5cdca3911e25c6fed69517ed26944090d04bf
Nel <n_wanyama@yahoo.com>
2022-12-19 10:30:51 +0000

    Count characters in string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** _strlen - calculate length of string
+ * @s: string to establish length of
+ *
+ * Return: length of string s
+ */
+
+int _strlen(char *s)
+{
+	char c = s[0];
+	int len = 0;
+
+	while (c != '\0')
+	{
+		len++;
+		c = s[len];
+	}
+
+	return (len);
+}
