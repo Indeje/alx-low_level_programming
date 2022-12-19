259e4158a9bf9704b61f0a5ab52410697ff0cb50
Nel <n_wanyama@yahoo.com>
2022-12-19 10:51:28 +0000

    Print string to terminal

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - print string
+ * @str: pointer to string to print
+ *
+ * Return: none
+ */
+
+void _puts(char *str)
+{
+	char c = *str, len = 0;
+
+	while (c != '\0')
+	{
+		_putchar(c);
+		len++;
+		c = *(str + len);
+	}
+	_putchar('\n');
+}
