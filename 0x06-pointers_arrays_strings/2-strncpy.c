10fe9ce7fd54e5332619138dd1055e32cfd12fa4
Nel <n_wanyama@yahoo.com>
2022-12-26 22:52:32 +0000

    Copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[count1] = src[count1];
 		count1++;
 	}
-	for(; count1 < n; count1++)
+	for (; count1 < n; count1++)
 		dest[count1] = '\0';
 
 	return (dest);
