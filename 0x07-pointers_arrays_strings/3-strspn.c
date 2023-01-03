29d7e6620fbcb4ae28cb8530d827d81cb8bd236d
Nale <n_wanyama@yahoo.com>
2023-01-03 20:40:35 +0000

    Search string for a set of bytes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _strspn - find length of initial segment of string containing only substring
+ * @s: original string
+ * @accept: substring
+ *
+ * Return: length of prefix substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0, i;
+
+	while (*(s + len) != '\0')
+	{
+		for (i = 0; i < _strlen(accept); i++)
+		{
+			if (*(s + len) == *(accept + i))
+				break;
+		}
+		if (*(s + len) != *(accept + i))
+			break;
+		len++;
+	}
+
+	return (len);
+}
+
+
+/**
+ * _strlen - find length of string
+ * @s: pointer to string
+ *
+ * Return: length of string in bytes
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
