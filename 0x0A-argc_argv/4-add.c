e81997160758f70193cbf6109a2e6cc7cbcfc55b
Nale <n_wanyama@yahoo.com>
2023-01-09 17:15:12 +0000

    Add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,27 +6,22 @@
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: always 0
+ * Return: 0 if successful, 1 if error
  */
 
 int main(int argc, char **argv)
 {
-	int i = 1, sum = 0, num;
-
-	if (argc == 1)
-	{
-		printf("%d\n", sum);
-		return (0);
-	}
+	int i = 1, j = 0, sum = 0;
 
 	for (; i < argc; i++)
 	{
-		num = atoi(argv[i]);
-
-		if (num <= 0)
+		for (j = 0; argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 
 		sum += atoi(argv[i]);
