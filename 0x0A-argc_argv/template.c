1d3ac4e826d74dba923dc372124d11f361ac0f45
Nale <n_wanyama@yahoo.com>
2023-01-09 16:16:23 +0000

    Multiply two numbers as CLI parameters

--- 0x0A-argc_argv/template.c
+++ 0x0A-argc_argv/template.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  * main - multiplies two number as arguments
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0 if success, else 1
+ * Return: always 0
  */
 
 int main(int argc, char **argv)
 {
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
-
 	return (0);
 }
