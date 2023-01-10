fd54408a9c4cc0cb4ea3aa6da9b0ccddff0b71fd
Nale <n_wanyama@yahoo.com>
2023-01-10 12:58:11 +0000

    Concatenate two strings and terminate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -36,3 +36,25 @@ char *str_concat(char *s1, char *s2)
 
 	return (0);
 }
+
+
+/**
+ * _strlen - calculate length of string
+ * @s: string to establish length of
+ *
+ * Return: length of string s
+ */
+
+int _strlen(char *s)
+{
+	char c = s[0];
+	int len = 0;
+
+	while (c != '\0')
+	{
+		len++;
+		c = s[len];
+	}
+
+	return (len);
+}
