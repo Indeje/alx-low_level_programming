97dc03e122502a66bf3fd2c446eb634f61f41405
Nale <n_wanyama@yahoo.com>
2023-01-12 02:34:08 +0000

    Allocate memory space to grid of integer and initialize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocates memory grid for 2D array of integer
+ * @width: width of grid
+ * @height: height of grid
+ *
+ * Return: pointer to grid if successful, else NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i = 0, j = 0, **ptr;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+
+	ptr = (int **) malloc(sizeof(int) * height);
+
+	if (ptr)
+	{
+		for (; i < height; i++)
+		{
+			ptr[i] = (int *) malloc(sizeof(int) * width);
+
+			if (ptr[i])
+			{
+				for (; j < width; j++)
+					ptr[i][j] = 0;
+			}
+			else
+			{
+				return (0);
+			}
+		}
+
+		return (ptr);
+	}
+
+	return (0);
+}
