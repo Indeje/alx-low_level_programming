752ba8ac726417df231fbbf9478a31cb6e9e2e8a
Nale <n_wanyama@yahoo.com>
2023-01-22 06:10:39 +0000

    Dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG.H
-#define DOG.H
+#ifndef DOG
+#define DOG
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
