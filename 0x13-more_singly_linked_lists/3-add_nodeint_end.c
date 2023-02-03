7ee1ad5679dacb45ff3941a2c03611fe57b51bab
Nale <n_wanyama@yahoo.com>
2023-02-03 23:08:12 +0000

    Add elements at end of linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,10 +7,34 @@
  * @head: linked list
  * @n: element to be added
  *
- * Return: Address pf the new element
+ * Return: Address pf the new element on success, else NULL
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	
+	listint_t *tmp, *newnode;
+
+	tmp = *head;
+	newnode = malloc(sizeof(listint_t));
+
+	if (newnode)
+	{
+		newnode->n = n;
+		newnode->next = NULL;
+
+		if (*head == NULL)
+		{
+			*head = newnode;
+			return (newnode);
+		}
+
+		while (tmp->next)
+			tmp = tmp->next;
+
+		tmp->next = newnode;
+
+		return (newnode);
+	}
+
+	return (NULL);
 }
